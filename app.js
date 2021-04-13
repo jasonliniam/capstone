128d117c60159e119e50b862d60b426549d75d91
Jason Lin <cjasonlin@gmail.com>
2021-04-13 11:18:53 -0700

    Set heroku deployment settings

--- app.js
+++ app.js
@@ -136,6 +136,8 @@ app.use((err, req, res, next) => {
 	res.status(status).render('errors', { err });
 });
 
-app.listen(3000, () => {
-	console.log('Running on port 3000');
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+	console.log(`Running on port ${port}`);
 });
